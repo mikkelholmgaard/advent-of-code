35afc3d9b724d97c2b6e8cdf3d5873bb487d2e4c
Mikkel Holmgaard <mikkel.holmgaard@lego.com>
2022-12-05 08:21:24 +0100

    Changed some var to val

--- 2022/day01.scala
+++ 2022/day01.scala
@@ -5,7 +5,7 @@ object Day01 {
     def main(args: Array[String]) = {
         var input: String = readLine
         var curr: Int = 0
-        var list: ListBuffer[Int] = new ListBuffer[Int]()
+        val list: ListBuffer[Int] = new ListBuffer[Int]()
 
         while (input != null) {
             if (input != "") {
@@ -17,7 +17,7 @@ object Day01 {
             input = readLine
         }
 
-        var sortedList = list.sortWith(_ > _)
+        val sortedList: ListBuffer[Int] = list.sortWith(_ > _)
 
         println(sortedList(0))
         println(sortedList(0) + sortedList(1) + sortedList(2))
