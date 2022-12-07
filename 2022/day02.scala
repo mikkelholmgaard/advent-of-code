c979baaca4cf030d5a7df575e57fbf0bd47be02e
Mikkel Holmgaard <mikkel.holmgaard@lego.com>
2022-12-07 20:40:33 +0100

    day02 part 2 solution

--- 2022/day02.scala
+++ 2022/day02.scala
@@ -2,24 +2,37 @@ import scala.io.StdIn.readLine
 import scala.collection.mutable.ListBuffer
 
 object Day02 {
-    def result(play: Array[String]): Int = {
-        play(0) match
-            case "A" => if (play(1) == "X") 4 else if (play(1) == "Y") 8 else 3
-            case "B" => if (play(1) == "X") 1 else if (play(1) == "Y") 5 else 9
-            case "C" => if (play(1) == "X") 7 else if (play(1) == "Y") 2 else 6
+    def result(play: Array[String], correctUse: Boolean): Int = {
+        correctUse match {
+            case false =>
+                play(0) match
+                    case "A" => if (play(1) == "X") 1+3 else if (play(1) == "Y") 2+6 else 0+3
+                    case "B" => if (play(1) == "X") 0+1 else if (play(1) == "Y") 2+3 else 3+6
+                    case "C" => if (play(1) == "X") 1+6 else if (play(1) == "Y") 0+2 else 3+3
+            case true => {
+                play(0) match
+                    case "A" => if (play(1) == "X") 0 + 3 else if (play(1) == "Y") 3 + 1 else 6 + 2
+                    case "B" => if (play(1) == "X") 0 + 1 else if (play(1) == "Y") 3 + 2 else 6 + 3
+                    case "C" => if (play(1) == "X") 0 + 2 else if (play(1) == "Y") 3 + 3 else 6 + 1
+            }
+        }
     }
 
     def main(args: Array[String]) = {
         var input = readLine
         var score = 0
+        var otherScore = 0
 
         while (input != null) {
             if (input != "") {
-                score += result(input.split(" "))
+                var x = input.split(" ")
+                score += result(x, false)
+                otherScore += result(x, true)
             }
             input = readLine
         }
 
         println(score)
+        println(otherScore)
     }
 }
