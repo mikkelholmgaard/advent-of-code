50203197d574587ad71a8049cf4418c9883fed05
Mikkel Holmgaard <mikkel.holmgaard@lego.com>
2022-12-08 07:36:58 +0100

    day03 first part

---
+++ 2022/day03.scala
@@ -0,0 +1,24 @@
+import scala.io.StdIn.readLine
+import scala.collection.mutable.ListBuffer
+
+object Day03 {
+    def main(args: Array[String]) = {
+        var input = readLine
+        var a = "abcdefghijklmnopqrstuvwxyz";
+        var aa = a + a.toUpperCase()
+        var priority = 0
+
+        while (input != null) {
+            if (input != "") {
+                var arr = input.splitAt(input.length/2)
+                var set1 = arr._1.toList.toSet
+                var set2 = arr._2.toList.toSet
+                var set3 = set1.intersect(set2)
+                priority += aa.indexOf(set3.head)+1
+            }
+            input = readLine
+        }
+
+        println(priority)
+    }
+}
